6fc8248ad3dadbe0a92f8c54b68d0a4a2bfd3e99
paula456 <2466776@cegep-heritage.qc.ca>
2026-07-18 23:15:16 -0400

    Add list feature (list all tasks in JSON file) and add status attribute

--- python/task-manager.py
+++ python/task-manager.py
@@ -1,10 +1,42 @@
 import json
+import time
 from pathlib import Path
+from dataclasses import dataclass, asdict
+from enum import Enum
 
 #add a new task to the json file
 
+class Status(Enum):
+    TODO = 1
+    IN_PROGRESS = 2
+    DONE = 3
+
+@dataclass
+class Task:
+    description: str
+    status: Enum
+
+
+
+#to be added later: status, date created, last updated date
+
+
+
 running = True
 valid_commands = ["add", "update", "delete"]
+
+#load all tasks
+def load(file):
+    path = Path(file)
+    tasks = {}
+
+    #if the path exists, read the file 
+    if path.exists():
+        with open(path, "r", encoding="utf-8") as file:
+            tasks = json.load(file)
+        return tasks
+    else:
+        return None
 
 
 
@@ -29,32 +61,47 @@ def add(c, t):
             tasks = json.load(file)
 
         index = len(tasks) + 1
-        tasks[index] = task
+
+        tasks[index] = asdict(Task(task, Status.TODO.name))
     else:
         #else, initialize to 1
-        tasks[1] = task
+        tasks[1] = asdict(Task(task, Status.TODO.name))
 
     append(path, tasks)
 
     
+print("Welcome to Task Manager! Please select a valid command.\n")
 
 while running:
     string = input()
 
     if string == "exit":
-        print("see ya!")
+        print("Saving...")
+        time.sleep(3)
+        print("Tasks saved. See you soon!")
         break
     else:
-        command, task = a, b = string.split(maxsplit=1)
+        words_num = len(string.split())
 
-        if (command not in valid_commands):
-            print("whoops! select a valid command (add, update, delete)")
+        if words_num > 1:
+            command, task = a, b = string.split(maxsplit=1)
         else:
-            print(f"Command: {command} Task: {task}")
-
+            command = string
 
         if command == "add":
             add(command, task)
+            print("Task added sucessfully")
+        elif command == "list":
+        
+            if load("data/tasks.json") is not None:
+
+                for id, task in load("data/tasks.json").items():
+                    print(f"{id}. {task.get("description")}, Status: {task.get("status")}", end=" ")
+                    print()
+            else:
+                print("whoops! no tasks yet. add one!")
+                
+        
                 
 
         
